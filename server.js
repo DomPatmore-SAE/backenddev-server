5496b33347a27fe2e0de325695701a756a09b381
Dom <dom@pilifter.com>
2021-10-07 14:55:27 +0100

    updated

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 const port = 3000
 const projects= [
-  {name:'Food Ordering form Single Restaurant'},
-  {name:'Commerce App'},
+
 ]
 
 app.get('/', (req, res) => {
@@ -19,12 +18,12 @@ app.get('/projects', (req,res)=>{
   res.json(projects)
 })
 app.post('/project',(req,res)=>{
-  console.log(`Project: ${req.body.title} `)
+  console.log(`Project: ${req.body.name} `)
   //get name and creator of project
   //create object and add to list
   //return list of projects
-  projects.push({name:req.body.title})
-  res.json(projects)
+  projects.push(req.body.name)
+  res.json(res.body.name)
 })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
